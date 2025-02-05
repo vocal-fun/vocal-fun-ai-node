8010d5d2333db45aaf65d92004dc099b0476f593
Naman Dwivedi <namandwivedi14@gmail.com>
2025-02-05 23:56:13 +0530

    update

--- tts.py
+++ tts.py
@@ -38,11 +38,8 @@ API_BASE = "https://api.elevenlabs.io/v1"
 os.environ['RVC_OUTPUTFREQ'] = '24000'  # Set your desired output sample rate
 
 print("Loading RVC model...")
-try:
- rvc_model = RVC(model='rvc/models/IShowSpeed/IShowSpeed.pth')
-except Exception as e:
-    print(f"Error loading RVC model: {e}")
-    rvc_model = None
+#rvc_model = RVC(model='rvc/models/IShowSpeed/IShowSpeed.pth')
+
 
 
 
