a964d04414402a06f02bc6c8606990abcb6d6da9
Naman Dwivedi <namandwivedi14@gmail.com>
2025-02-06 00:24:48 +0530

    update

--- tts.py
+++ tts.py
@@ -37,7 +37,7 @@ os.environ['RVC_INDEXDIR'] = 'rvc/models'
 os.environ['RVC_OUTPUTFREQ'] = '24000'  # Set your desired output sample rate
 
 print("Loading RVC model...")
-rvc_model = RVC('IShowSpeed/IShowSpeed.pth', index='IShowSpeed/IShowSpeed.index')
+rvc_model = None
 print("RVC model loaded")
 
 class CartesiaWebSocketManager:
@@ -440,6 +440,8 @@ async def stream_audio_chunks_elevenlabs(websocket: WebSocket, text: str, person
 @app.websocket("/tts/stream")
 async def tts_stream(websocket: WebSocket):
     """Original XTTS WebSocket endpoint"""
+    rvc_model = RVC('IShowSpeed/IShowSpeed.pth', index='IShowSpeed/IShowSpeed.index')
+
     if not ENABLE_LOCAL_MODEL:
         await websocket.close(code=1000, reason="Local model is disabled")
         return
