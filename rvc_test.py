b69f618ea335f0a9d640786063189c7fd43ba730
Naman Dwivedi <namandwivedi14@gmail.com>
2025-02-14 16:11:08 +0530

    remove unused files

--- rvc_test.py
+++
@@ -1,9 +0,0 @@
-from inferrvc import RVC
-import os
-
-os.environ['RVC_MODELDIR'] = 'rvc/models'
-os.environ['RVC_INDEXDIR'] = 'rvc/models'
-
-print("Loading RVC model...")
-rvc_model = RVC('IShowSpeed/IShowSpeed.pth', index='IShowSpeed/IShowSpeed.index')
-print("RVC model loaded")
