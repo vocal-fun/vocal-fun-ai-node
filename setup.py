5413ab6e8ec263996cfc24fbb440f7ffca7fb18c
Naman Dwivedi <namandwivedi14@gmail.com>
2025-03-08 02:14:32 +0530

    update setup.py with specific deps

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="vocal",
-    version="0.1",
+    name="vocal",  # Replace with your project name
+    version="0.1.0",
     packages=find_packages(),
+    install_requires=[
+        "torch==2.5.1",
+        "torchvision==0.20.1",
+        "torchaudio==2.5.1",
+        "vllm==0.7.2",
+        "transformers==4.46.2",
+        "deepspeed==0.16.4",
+        "pydantic==2.10.6",
+        "coqui-tts==0.25.3",
+    ],
+    python_requires=">=3.10",
 )
